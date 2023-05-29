30865d18465229f09b988f65a88b9ec2df0ded2b
“marylishop” <marylishop@yahoo.com>
2023-05-29 20:10:32 +0400

    Update to pull

--- _printf.c
+++ _printf.c
@@ -1,4 +1,4 @@
-#include "main.h"
+
 #include <stddef.h>
 #include <stdarg.h>
 
--- main.h
+++ main.h
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-
-=======
 #ifndef MAIN_H
 
 #define MAIN_H
@@ -98,4 +95,3 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 #endif /* MAIN_H */
->>>>>>> cd81f6a8fefe7d21b6b6f3f3000e8de5c7b91169
